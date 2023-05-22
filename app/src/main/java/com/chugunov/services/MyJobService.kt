995027025ea4ada02eceee11e15f28b73920e123
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-05-23 00:52:18 +0300

    creating JobService

---
+++ app/src/main/java/com/chugunov/services/MyJobService.kt
@@ -0,0 +1,48 @@
+package com.chugunov.services
+
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService: JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        coroutineScope.launch {
+            log("onStartCommand")
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer: $i")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutineScope.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "My service: $message")
+    }
+}
