b3de4842694823a502efb801327cfbcf0bef7fbf
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-05-23 00:41:35 +0300

    creating IntentService

--- app/src/main/java/com/chugunov/services/MainActivity.kt
+++ app/src/main/java/com/chugunov/services/MainActivity.kt
@@ -28,5 +28,8 @@ class MainActivity : AppCompatActivity() {
         binding.foregroundService.setOnClickListener {
             ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
         }
+        binding.intentService.setOnClickListener {
+            ContextCompat.startForegroundService(this, MyIntentService.newIntent(this))
+        }
     }
 }
