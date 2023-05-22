bc9fde08eea62cf1bd9b424a10672dc5542db55e
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-05-22 19:02:01 +0300

    created service

--- app/src/main/java/com/chugunov/services/MainActivity.kt
+++ app/src/main/java/com/chugunov/services/MainActivity.kt
@@ -3,8 +3,11 @@ package com.chugunov.services
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chugunov.services.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class MainActivity : AppCompatActivity() {
+
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -13,5 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
---
+++ app/src/main/java/com/chugunov/services/MyService.kt
@@ -0,0 +1,50 @@
+package com.chugunov.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyService: Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        coroutineScope.launch {
+            log("onStartCommand")
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer: $i")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutineScope.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "My service: $message")
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyService::class.java)
+        }
+    }
+
+}
