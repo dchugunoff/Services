702084a56cd4715b29ae65e6d888a65b7fcfcd98
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-05-23 16:21:12 +0300

    creating enqueue jobservice

--- app/src/main/java/com/chugunov/services/MyJobService.kt
+++ app/src/main/java/com/chugunov/services/MyJobService.kt
@@ -1,27 +1,34 @@
 package com.chugunov.services
 
-import android.app.Service
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
 import android.content.Intent
-import android.os.IBinder
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.*
 
-class MyJobService: JobService() {
+class MyJobService : JobService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        coroutineScope.launch {
-            log("onStartCommand")
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer: $i")
+        log("onStartCommand")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer: $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -48,5 +55,12 @@ class MyJobService: JobService() {
 
     companion object {
         const val JOB_ID = 123
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
